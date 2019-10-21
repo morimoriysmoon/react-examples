8466cac2084d30c71a6f62b6ca3ec8af731f2d30
Yongsik Moon <morimori.ysmoon@gmail.com>
2019-10-21 19:59:08 +1100

    add css file

---
+++ calculator/src/components/VersionDisp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VersionDisp = ({versionNo}) => {
+    return <div>
+        <span>Version</span>{' '}<span><strong>{versionNo}</strong></span>
+    </div>
+}
+
+export default VersionDisp;
